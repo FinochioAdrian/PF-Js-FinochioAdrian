ea11c3c56ed3694ec9b2f31b77dfee9a26a96096
FinochioAdrian <eidrienhez33@gmail.com>
2023-10-07 14:01:53 -0300

    [Feat] add page Nuevo Producto

--- src/js/components/productosdb.js
+++ src/js/components/productosdb.js
@@ -1,63 +1,67 @@
 import { URLAPI } from "../services/constantes.js";
 
-
-
 export async function recuperarCatalogo() {
-    let res
- return res= await fetch(URLAPI+"productos")
-    .then(response => response.json())
-    .then(data => data)
-     
+  let res;
+  return (res = await fetch(URLAPI + "productos")
+    .then((response) => response.json())
+    .then((data) => data));
 }
 //recupera un producto
 export async function recuperarProducto(id) {
-    let res
- return res= await fetch(URLAPI+`productos/${id}`)
-    .then(response => response.json())
-    .then(data => data)
-     
+  let res;
+  return (res = await fetch(URLAPI + `productos/${id}`)
+    .then((response) => response.json())
+    .then((data) => data));
 }
 
 /* Funcion que recupera y carga los productos almacenados en memoria en la db */
 async function subirCatalogo() {
-    const productoLocales=  await cargarJsonlocal()
-    
-    for (const producto of productoLocales) {
-        await subirProducto(producto);
-    }
+  const productoLocales = await cargarJsonlocal();
+    const promesas = productoLocales.map(producto => subirProducto(producto))
+try {
 
-} 
+    const resultados = await Promise.all(promesas)
+}catch (error) {
+    // Manejar errores si es necesario
+    console.error("Error al enviar los productos:", error);
+  }
+ 
+}
 
 /* recupera los productos en memoria servidor */
 async function cargarJsonlocal() {
-   const resp = await fetch("./src/js/products.json")
-        .then(response => response.json())
-        .then(data => data)
-    return resp
-
+  const resp = await fetch("./src/js/products.json")
+    .then((response) => response.json())
+    .then((data) => data);
+  return resp;
 }
 /* Encargada de cargar el catalogo en la memoria actual  catalogoArr*/
-export async function cargarCatalogo (arr){
-    const res = await recuperarCatalogo()
-    if (res.length>0){
-        arr.push(...res)
-    
-    }else {
-     await subirCatalogo()
-     await cargarCatalogo(arr);
-
-     
-    }
+export async function cargarCatalogo(arr) {
+  const res = await recuperarCatalogo();
+  if (res.length > 0) {
+    arr.push(...res);
+  } else {
+    await subirCatalogo();
+    await cargarCatalogo(arr);
+  }
 }
 
 /* Sube un producto a la db */
-export async function subirProducto(producto) {
-    const resp = await fetch(URLAPI+"productos", {
-        method: "POST",
-        body: JSON.stringify(producto),
-        headers: {
-            "Content-Type": "application/json"
-        }
+export function subirProducto(producto) {
+  return fetch(URLAPI + "productos", {
+    method: "POST",
+    body: JSON.stringify(producto),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((resultado) => {
+      return resultado;
     })
-    const data = await resp.json()
+    .catch((error) => {
+      // Manejar errores si es necesario
+      console.error("Error al enviar el producto:", error);
+      throw error;
+    });
 }
--- src/js/pages/gestionarProducto.js
+++ src/js/pages/gestionarProducto.js
@@ -1,196 +1,223 @@
+import { subirProducto } from "../components/productosdb.js";
 
-  export default function gestionarProducto() {
-    const formProductos = document.querySelector("#form-productos");
-    const formInputs = document.querySelectorAll(".form-inputs");
-    const productos = [];
-    document.addEventListener(
-      "DOMContentLoaded",
-      () => {
-        
-  
-        document.addEventListener("keypress", (e) => {
-          e.key === "Enter" && validacionFormulario();
-        });
-  
-        formProductos.addEventListener("submit", (e) => {
+export default function gestionarProducto() {
+  const formProductos = document.querySelector("#form-productos");
+  const formInputs = document.querySelectorAll(".form-inputs");
+  const productos = [];
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+      document.addEventListener("keypress", (e) => {
+        e.key === "Enter" && validacionFormulario();
+      });
+
+      formProductos.addEventListener("submit", (e) => {
+        e.preventDefault();
+        validacionFormulario();
+      });
+      /* escucha los cambios en el formulario */
+      formInputs.forEach((value) => {
+        value.addEventListener("change", (e) => {
           e.preventDefault();
-          validacionFormulario();
-        });
-        /* escucha los cambios en el formulario */
-        formInputs.forEach((value) => {
-          value.addEventListener("change", (e) => {
-            e.preventDefault();
-            formInputs.forEach((input) => {
-              if (e.target == input) {
-                
-                if (validacionesFormulario.hasOwnProperty(input.name)) {
-                  const rules = validacionesFormulario[input.name];
-  
-                  if (!reglasDeValidaciones(rules, input.value)) {
-                    input.classList.add("is-invalid");
-                    input.classList.remove("is-valid");
-                  } else {
-                    input.classList.remove("is-invalid");
-                    input.classList.add("is-valid");
-                  }
+          formInputs.forEach((input) => {
+            if (e.target == input) {
+              if (validacionesFormulario.hasOwnProperty(input.name)) {
+                const rules = validacionesFormulario[input.name];
+
+                if (!reglasDeValidaciones(rules, input.value)) {
+                  input.classList.add("is-invalid");
+                  input.classList.remove("is-valid");
+                } else {
+                  input.classList.remove("is-invalid");
+                  input.classList.add("is-valid");
                 }
               }
-            });
+            }
           });
         });
-      },
-      { once: true }
-    );
-  
-   
+      });
+    },
+    { once: true }
+  );
+
   /* Reglas de validaciones del formulario, el primer atributo coincide con el atributo name de la etiqueta html input a validar, */
-    const validacionesFormulario = {
-        nombre: {
-        required: true,
-        type: "text",
-        msgInvalid: "Debe de ingresar un nombre",
-      },
-      precio: {
-        required: true,
-        type: "number",
-        min: 1,
-        msgInvalid: "Debe de ingresar un precio",
-      },
-      stock: {
-        required: true,
-        type: "number",
-        min: 1,
-        msgInvalid: "Debe de ingresar un nombre",
-      },
-      categoria: {
-        required: true,
-        type: "select",
-        msgInvalid: "Debe de ingresar un nombre",
-      },
-      talle: {
-        required: true,
-        type: "select",
-        msgInvalid: "Debe de ingresar un talle",
-      },
-      estado: {
-        required: true,
-        type: "select",
-        msgInvalid: "Debe de ingresar un estado",
-      },
-      descripcion: {
-        required: true,
-        type: "text",
-        msgInvalid: "Debe de ingresar un nombre",
-      },
-      textarea: {
-        required: true,
-        type: "text",
-        msgInvalid: "Debe de ingresar un nombre",
-      },
-    };
-    /* Definicon de las reglas de validaciones */
-    function reglasDeValidaciones(rules, value) {
-      let res;
-      switch (rules.type) {
-        case "text":
-          res = reglaValidacionesText(value, rules);
-          return res;
-        case "number":
-          res = reglaValidacionesNumber(value, rules);
-          return res;
-        case "select":
-          res = reglaValidacionesSelect(value, rules);
-          return res;
-        default:
-          console.log("El tipo no existe");
-          break;
+  const validacionesFormulario = {
+    nombre: {
+      required: true,
+      type: "text",
+      msgInvalid: "Debe de ingresar un nombre",
+    },
+    precio: {
+      required: true,
+      type: "number",
+      min: 1,
+      msgInvalid: "Debe de ingresar un precio",
+    },
+    stock: {
+      required: true,
+      type: "number",
+      min: 1,
+      msgInvalid: "Debe de ingresar un nombre",
+    },
+    categoria: {
+      required: true,
+      type: "select",
+      msgInvalid: "Debe de ingresar un nombre",
+    },
+    talle: {
+      required: true,
+      type: "text",
+      msgInvalid: "Debe de ingresar un talle",
+    },
+    estado: {
+      required: true,
+      type: "select",
+      msgInvalid: "Debe de ingresar un estado",
+    },
+    descripcion: {
+      required: true,
+      type: "text",
+      msgInvalid: "Debe de ingresar un nombre",
+    },
+    textarea: {
+      required: true,
+      type: "text",
+      msgInvalid: "Debe de ingresar un nombre",
+    },
+  };
+  /* Definicion de las reglas de validaciones */
+  function reglasDeValidaciones(rules, value) {
+    let res;
+    switch (rules.type) {
+      case "text":
+        res = reglaValidacionesText(value, rules);
+        return res;
+      case "number":
+        res = reglaValidacionesNumber(value, rules);
+        return res;
+      case "select":
+        res = reglaValidacionesSelect(value, rules);
+        return res;
+      default:
+        console.log("El tipo no existe");
+        break;
+    }
+  }
+  /* defincion las reglas para texto */
+  function reglaValidacionesText(value, rules) {
+    const ArrIsValid = [];
+    for (const key in rules) {
+      if (key == "required" && rules[key]) {
+        ArrIsValid.push(value.trim().length > 0);
+      }
+      if (key == "type") {
+        ArrIsValid.push(typeof value == "string");
       }
     }
-    /* defincion las reglas para texto */
-    function reglaValidacionesText(value, rules) {
-      const ArrIsValid = [];
-      for (const key in rules) {
-        if (key == "required" && rules[key]) {
-          ArrIsValid.push(value.trim().length > 0);
-        }
-        if (key == "type") {
-          ArrIsValid.push(typeof value == "string");
-        }
+
+    return !ArrIsValid.some((value) => value == false);
+  }
+  /* defincion las reglas para Selects */
+
+  function reglaValidacionesSelect(value, rules) {
+    const ArrIsValid = [];
+    for (const key in rules) {
+      if (key == "required" && rules[key]) {
+        ArrIsValid.push(value !== "-1");
       }
-  
-      return !ArrIsValid.some((value) => value == false);
     }
-    /* defincion las reglas para Selects */
-  
-    function reglaValidacionesSelect(value, rules) {
-      const ArrIsValid = [];
-      for (const key in rules) {
-        if (key == "required" && rules[key]) {
-          ArrIsValid.push(value !== "-1");
-        }
+
+    return !ArrIsValid.some((value) => value == false);
+  }
+
+  /* defincion las reglas para numeros */
+
+  function reglaValidacionesNumber(value, rules) {
+    const ArrIsValid = [];
+    for (const key in rules) {
+      if (key == "required" && rules[key]) {
+        ArrIsValid.push(value !== "");
       }
-  
-      return !ArrIsValid.some((value) => value == false);
-    }
-    /* defincion las reglas para numeros */
-  
-    function reglaValidacionesNumber(value, rules) {
-      const ArrIsValid = [];
-      for (const key in rules) {
-        if (key == "required" && rules[key]) {
-          ArrIsValid.push(value !== "");
-        }
-        if (key == "type") {
-          ArrIsValid.push(!isNaN(Number(value)));
-        }
-        if (key == "min") {
-          ArrIsValid.push(Number(value) >= rules[key]);
-        }
+      if (key == "type") {
+        ArrIsValid.push(!isNaN(Number(value)));
       }
-      return !ArrIsValid.some((value) => value == false);
+      if (key == "min") {
+        ArrIsValid.push(Number(value) >= rules[key]);
+      }
     }
-  
-   
-    /* Funcion que se llama al enviar el formulario, Valida cada campo con su regla establecida en la variable validacionesFormulario
+    return !ArrIsValid.some((value) => value == false);
+  }
+
+  /* Funcion que se llama al enviar el formulario, Valida cada campo con su regla establecida en la variable validacionesFormulario
     Esta funcion llama a la validación y alerta de campos vacios
     ademas crea los objetos
     */
-    function validacionFormulario() {
-      const isFormValid = [];
-      formInputs.forEach((input) => {
-        if (validacionesFormulario.hasOwnProperty(input.name)) {
-          const rules = validacionesFormulario[input.name];
-  
-          if (!reglasDeValidaciones(rules, input.value)) {
-            input.classList.add("is-invalid");
-            input.classList.remove("is-valid");
-            isFormValid.push(false);
-          } else {
-            input.classList.remove("is-invalid");
-            input.classList.add("is-valid");
-            isFormValid.push(true);
-          }
+  function validacionFormulario() {
+    const isFormValid = [];
+    formInputs.forEach((input) => {
+      if (validacionesFormulario.hasOwnProperty(input.name)) {
+        const rules = validacionesFormulario[input.name];
+
+        if (!reglasDeValidaciones(rules, input.value)) {
+          input.classList.add("is-invalid");
+          input.classList.remove("is-valid");
+          isFormValid.push(false);
+        } else {
+          input.classList.remove("is-invalid");
+          input.classList.add("is-valid");
+          isFormValid.push(true);
         }
-      });
-      if (!isFormValid.some((value) => value == false)) {
-        const nuevoProducto = Object.fromEntries(new FormData(formProductos));
-        console.log(nuevoProducto);
-        //Todo seguir aqui
-  
-        
-        limpiarCampos()
-      } else {
-        alert("Hay campos vacios");
       }
-    }
-    
-    function limpiarCampos() {
-      formProductos.reset();
-      formInputs.forEach((input) => {
-        input.classList.remove("is-invalid");
-        input.classList.remove("is-valid");
-  
-      });
+    });
+    if (!isFormValid.some((value) => value == false)) {
+      const nuevoProducto = Object.fromEntries(new FormData(formProductos));
+
+      if (nuevoProducto.imagen == "") {
+        nuevoProducto.imagen = "https://via.placeholder.com/150";
+      }
+      console.log(nuevoProducto);
+      
+      try {
+        const res = subirProducto(nuevoProducto);
+        toastCustom("Producto guardado", "info");
+        limpiarCampos();
+      } catch (error) {
+        // Manejar errores si es necesario
+        toastCustom("Error al enviar el producto", "warning");
+        console.error("Error al enviar el producto:", error);
+        throw error;
+      }
+    } else {
+      toastCustom("Hay campos vacios", "warning");
     }
   }
-  
+  /* limpia todos lo campos del formulario */
+  function limpiarCampos() {
+    formProductos.reset();
+    formInputs.forEach((input) => {
+      input.classList.remove("is-invalid");
+      input.classList.remove("is-valid");
+    });
+  }
+}
+
+function toastCustom(text, status = "info") {
+  const warning =
+    "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))";
+  const info = "linear-gradient(to right, #00b09b, #96c93d)";
+  Toastify({
+    text: text,
+    duration: 3000,
+    className: "warning",
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: false, // Prevents dismissing of toast on hover
+    style: {
+      background: status != "info" ? warning : info,
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+}
+
