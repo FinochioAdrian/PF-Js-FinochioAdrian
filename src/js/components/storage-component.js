79bde682e68c154bfbe3e2758f5dffbfbd0bc136
FinochioAdrian <eidrienhez33@gmail.com>
2023-10-06 22:33:15 -0300

    Eliminando Para restablecer trabajo. Un error no deja usar el carrito de compras en el menu de inicio

--- src/js/components/storage-component.js
+++
@@ -1,9 +0,0 @@
-
-
-export function guardarStorage (clave,valor) {
-localStorage.setItem(clave,JSON.stringify(valor))
-}
-
-export function recuperarStorage (clave){
-    return localStorage.getItem(clave)
-}
--- src/js/services/constantes.js
+++
@@ -1,4 +0,0 @@
-
-
-export const CELULAR = "+5492995530612";
-export const URLAPI = "https://651437ed8e505cebc2eaf519.mockapi.io/abitiUsati/api/v1/"
